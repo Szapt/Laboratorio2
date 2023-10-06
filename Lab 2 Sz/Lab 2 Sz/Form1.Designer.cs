0f5dcb7ed86be16adfd1fc8d4b9d82c1d4f82409
Szapt <santiagod.zapata@udea.edu.co>
2023-10-05 19:36:57 -0500

    el segundo

---
+++ Lab 2 Sz/Lab 2 Sz/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Lab_2_Sz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Subtitulo = new System.Windows.Forms.Label();
+            this.opcion = new System.Windows.Forms.ComboBox();
+            this.fRecta1 = new System.Windows.Forms.TextBox();
+            this.fRecta2 = new System.Windows.Forms.TextBox();
+            this.fPuntoPend1 = new System.Windows.Forms.TextBox();
+            this.fPuntoPend2 = new System.Windows.Forms.TextBox();
+            this.btnObtener = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Subtitulo
+            // 
+            this.Subtitulo.AutoSize = true;
+            this.Subtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subtitulo.Location = new System.Drawing.Point(103, 32);
+            this.Subtitulo.Name = "Subtitulo";
+            this.Subtitulo.Size = new System.Drawing.Size(534, 24);
+            this.Subtitulo.TabIndex = 0;
+            this.Subtitulo.Text = "Selecciona la forma en la que quieres escribir tus rectas";
+            // 
+            // opcion
+            // 
+            this.opcion.FormattingEnabled = true;
+            this.opcion.Items.AddRange(new object[] {
+            "y = mx+b",
+            "y-yo = m(x-xo)"});
+            this.opcion.Location = new System.Drawing.Point(304, 59);
+            this.opcion.Name = "opcion";
+            this.opcion.Size = new System.Drawing.Size(121, 21);
+            this.opcion.TabIndex = 1;
+            this.opcion.Text = "Ecuacion";
+            this.opcion.SelectedIndexChanged += new System.EventHandler(this.opcionEleg);
+            // 
+            // fRecta1
+            // 
+            this.fRecta1.Location = new System.Drawing.Point(292, 108);
+            this.fRecta1.Name = "fRecta1";
+            this.fRecta1.Size = new System.Drawing.Size(148, 20);
+            this.fRecta1.TabIndex = 2;
+            this.fRecta1.Text = "Ingresa tu primera recta";
+            this.fRecta1.Visible = false;
+            this.fRecta1.ForeColorChanged += new System.EventHandler(this.fRecta1_Textcolor);
+            this.fRecta1.TextChanged += new System.EventHandler(this.fRecta1_TextChanged_1);
+            this.fRecta1.Enter += new System.EventHandler(this.fRecta1_Enter);
+            this.fRecta1.Leave += new System.EventHandler(this.fRecta1_Leave);
+            // 
+            // fRecta2
+            // 
+            this.fRecta2.Location = new System.Drawing.Point(292, 134);
+            this.fRecta2.Name = "fRecta2";
+            this.fRecta2.Size = new System.Drawing.Size(148, 20);
+            this.fRecta2.TabIndex = 3;
+            this.fRecta2.Text = "Ingresa tu segunda recta";
+            this.fRecta2.Visible = false;
+            this.fRecta2.Click += new System.EventHandler(this.fRecta2_TextChanged);
+            this.fRecta2.Enter += new System.EventHandler(this.fRecta2_Enter);
+            this.fRecta2.Leave += new System.EventHandler(this.fRecta2_Leave);
+            // 
+            // fPuntoPend1
+            // 
+            this.fPuntoPend1.Location = new System.Drawing.Point(292, 160);
+            this.fPuntoPend1.Name = "fPuntoPend1";
+            this.fPuntoPend1.Size = new System.Drawing.Size(148, 20);
+            this.fPuntoPend1.TabIndex = 4;
+            this.fPuntoPend1.Text = "Ingresa tu primera recta";
+            this.fPuntoPend1.Visible = false;
+            this.fPuntoPend1.Enter += new System.EventHandler(this.fPuntoPend1_Enter);
+            this.fPuntoPend1.Leave += new System.EventHandler(this.fPuntoPend1_Leave);
+            // 
+            // fPuntoPend2
+            // 
+            this.fPuntoPend2.Location = new System.Drawing.Point(292, 186);
+            this.fPuntoPend2.Name = "fPuntoPend2";
+            this.fPuntoPend2.Size = new System.Drawing.Size(148, 20);
+            this.fPuntoPend2.TabIndex = 5;
+            this.fPuntoPend2.Text = "Ingresa tu segunda recta";
+            this.fPuntoPend2.Visible = false;
+            this.fPuntoPend2.Enter += new System.EventHandler(this.fPuntoPend2_Enter);
+            this.fPuntoPend2.Leave += new System.EventHandler(this.fPuntoPend2_Leave);
+            // 
+            // btnObtener
+            // 
+            this.btnObtener.BackColor = System.Drawing.Color.LightGray;
+            this.btnObtener.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObtener.Location = new System.Drawing.Point(326, 228);
+            this.btnObtener.Name = "btnObtener";
+            this.btnObtener.Size = new System.Drawing.Size(75, 23);
+            this.btnObtener.TabIndex = 6;
+            this.btnObtener.Text = "Obtener";
+            this.btnObtener.UseVisualStyleBackColor = false;
+            this.btnObtener.Visible = false;
+            this.btnObtener.Click += new System.EventHandler(this.btnObtener_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(261, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "esperando que hagas algo...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 52);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Advertencias:\r\n-Si no seleccionas una forma, no podrás encontrar \r\n- Si quieres i" +
+    "ngresar un decimal recuerda usar coma y no punto\r\n\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnObtener);
+            this.Controls.Add(this.fPuntoPend2);
+            this.Controls.Add(this.fPuntoPend1);
+            this.Controls.Add(this.fRecta2);
+            this.Controls.Add(this.fRecta1);
+            this.Controls.Add(this.opcion);
+            this.Controls.Add(this.Subtitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Subtitulo;
+        private System.Windows.Forms.ComboBox opcion;
+        private System.Windows.Forms.TextBox fRecta1;
+        private System.Windows.Forms.TextBox fRecta2;
+        private System.Windows.Forms.TextBox fPuntoPend1;
+        private System.Windows.Forms.TextBox fPuntoPend2;
+        private System.Windows.Forms.Button btnObtener;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab 2 Sz/Lab 2 Sz/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2_Sz
+{
+    public partial class Form1 : Form
+    {
+        
+        string rectaIng1 = "y=mx+b";
+        string rectaIng2 = "y=mx+b";
+        string rectaPIng1 = "y=mx+b";
+        string rectaPIng2 = "y=mx+b";
+        int longitud = 0;
+        float m1=0;
+        float m2=0;
+        float b1 = 0;
+        float b2 = 0;
+        double resulNum = -1111111111;
+
+        
+
+        public string pendienteString(int indexMRec, int indexXRec, string rectaIng)
+        {
+            int recorrido = 0;
+            recorrido = indexXRec - indexMRec + 1;
+            string pendienteString = rectaIng.Substring(indexMRec, recorrido);
+            return pendienteString;
+        }
+        public string interceptoString(int indexMasRec, int length, string rectaIng)
+        {
+            int recorrido2 = length - indexMasRec;
+
+            //sumamos +3 en indexX ya que rectaIng1.Length empieza desde 1 y no 0 e indexX se encuentra dos casillas antes de el primer valor de b 
+            string intercepString = rectaIng.Substring(indexMasRec,recorrido2);
+           return intercepString;
+        }
+
+        public double interseccion(float m1,float m2,float b1, float b2)
+        {
+            return (b2-b1)/(m1-m2);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string txtPredet1 = "Ingresa tu primera recta";
+        string txtPredet2 = "Ingresa tu segunda recta";
+        private void opcionEleg(object sender, EventArgs e)
+        {
+            if (opcion.SelectedItem.ToString() == "y = mx+b")
+            {
+                // Mostrar los elementos Label para "m" y "b"
+                fRecta1.Visible = true;
+                fRecta2.Visible = true;
+                btnObtener.Visible = true;
+                fPuntoPend1.Visible = false;
+                fPuntoPend2.Visible = false;
+
+
+                               
+            }
+            else if (opcion.SelectedItem.ToString() == "y-yo = m(x-xo)")
+            {
+                // Mostrar los elementos Label para "m" y "b"
+                fRecta1.Visible = false;
+                fRecta2.Visible = false;
+                btnObtener.Visible = true;
+                fPuntoPend1.Visible = true;
+                fPuntoPend2.Visible = true;
+            }
+        }
+       
+
+        private void btnObtener_Click(object sender, EventArgs e)
+        {
+            if(opcion.SelectedItem.ToString() == "y = mx+b")
+            {
+                if (fRecta1.Text != txtPredet1 && fRecta2.Text != txtPredet2)
+                {
+                    rectaIng1 = fRecta1.Text;
+                    rectaIng2 = fRecta2.Text;
+                    rectaPIng1 = fPuntoPend1.Text;
+                    rectaPIng2 = fPuntoPend2.Text;
+
+                    int longitud1 = rectaIng1.Length;
+                    int indexM1 = rectaIng1.IndexOf("=") + 1;
+                    int indexX1 = rectaIng1.IndexOf("x") - 1;
+                    int indexMas1 = rectaIng1.IndexOf("+");
+                    String pendString1 = pendienteString(indexM1, indexX1, rectaIng1);
+                    string intercepString1 = interceptoString(indexMas1, longitud1, rectaIng1);
+                     m1 = float.Parse(pendString1);
+                     b1 = float.Parse(intercepString1);
+
+                    int longitud2 = rectaIng2.Length;
+                    int indexM2 = rectaIng2.IndexOf("=") + 1;
+                    int indexX2 = rectaIng2.IndexOf("x") - 1;
+                    int indexMas2 = rectaIng2.IndexOf("+");
+                    string pendString2 = pendienteString(indexM2, indexX2, rectaIng2);
+                    string intercepString2 = interceptoString(indexMas2, longitud2, rectaIng2);
+                     m2 = float.Parse(pendString2);
+                     b2 = float.Parse(intercepString2);
+                    string resultado = "no entendi marce";
+                    if (m2 == m1)
+                    {
+                        resultado = "las rectas no se intersectan, ¡son paralelas!";
+                    }
+                    else 
+                    {
+                        resulNum = interseccion(m1, m2, b1, b2);
+                        bool perpend = (m1 * m2 == -1);
+
+                        if (perpend)
+                        {
+                            resultado = "Las rectas son perpendiculares y se intersectan en x: " + resulNum;
+                        }
+                        else
+                        {
+                            resultado = "Las rectas se intersectan en x: " + resulNum;
+                        }
+                    }
+                    
+
+                    //Esto es solo de prueba, regresa cambiarlo a el resul luego
+                    label1.Text = resultado;
+                }
+               
+
+            }
+            else if (opcion.SelectedItem.ToString() == "y-yo = m(x-xo)")
+            {
+
+            }
+            else
+            {
+                label1.Text = "eror";
+            }
+
+
+
+        }
+       
+
+
+        private void fRecta1_Enter(object sender, EventArgs e)
+        {
+            // Borra el texto predeterminado cuando el usuario hace clic en el TextBox
+            if (fRecta1.Text == txtPredet1)
+            {
+                fRecta1.Text = "";
+            }
+        }
+
+        private void fRecta1_Leave(object sender, EventArgs e)
+        {
+            // Restaura el texto predeterminado si el TextBox está vacío cuando el usuario sale de él
+            if (string.IsNullOrWhiteSpace(fRecta1.Text))
+            {
+                fRecta1.Text = txtPredet1;
+            }
+        }
+
+        private void fRecta2_Enter(object sender, EventArgs e)
+        {
+            // Borra el texto predeterminado cuando el usuario hace clic en el TextBox
+            if (fRecta2.Text == txtPredet2)
+            {
+                fRecta2.Text = "";
+            }
+        }
+
+        private void fRecta2_Leave(object sender, EventArgs e)
+        {
+            // Restaura el texto predeterminado si el TextBox está vacío cuando el usuario sale de él
+            if (string.IsNullOrWhiteSpace(fRecta2.Text))
+            {
+                fRecta2.Text = txtPredet2;
+            }
+        }
+        private void fPuntoPend1_Enter(object sender, EventArgs e)
+        {
+            // Borra el texto predeterminado cuando el usuario hace clic en el TextBox
+            if (fPuntoPend1.Text == txtPredet1)
+            {
+                fPuntoPend1.Text = "";
+            }
+        }
+
+        private void fPuntoPend1_Leave(object sender, EventArgs e)
+        {
+            // Restaura el texto predeterminado si el TextBox está vacío cuando el usuario sale de él
+            if (string.IsNullOrWhiteSpace(fPuntoPend1.Text))
+            {
+                fPuntoPend1.Text = txtPredet1;
+            }
+        }
+
+        private void fPuntoPend2_Enter(object sender, EventArgs e)
+        {
+            // Borra el texto predeterminado cuando el usuario hace clic en el TextBox
+            if (fPuntoPend2.Text == txtPredet2)
+            {
+                fPuntoPend2.Text = "";
+            }
+        }
+
+        private void fPuntoPend2_Leave(object sender, EventArgs e)
+        {
+            // Restaura el texto predeterminado si el TextBox está vacío cuando el usuario sale de él
+            if (string.IsNullOrWhiteSpace(fPuntoPend2.Text))
+            {
+                fPuntoPend2.Text = txtPredet2;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void fRecta1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void fRecta2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void fRecta1_Textcolor(object sender, EventArgs e)
+        {}
+
+        private void fRecta1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
